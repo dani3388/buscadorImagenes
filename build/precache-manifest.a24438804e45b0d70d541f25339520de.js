82a8a919955312136ca03588b9b013431ff9e3f0
dani3388 <valenciaguzmand@gmail.com>
2020-12-15 16:00:40 -0500

    prueba

---
+++ build/precache-manifest.a24438804e45b0d70d541f25339520de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ae3c5474af2f4f2685abbd5f51bedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c961bdea68c07082ae7d",
+    "url": "/static/js/2.9dee5255.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9dee5255.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2f3b5fa2d65d95b0bee",
+    "url": "/static/js/main.128ec1c8.chunk.js"
+  },
+  {
+    "revision": "17b75e78d3c37da123de",
+    "url": "/static/js/runtime-main.4011744d.js"
+  }
+]);
